7e511c5e23d243fc4a5ed0823b90e4910ad3c953
JoakyEinstein <e.jmanosalvas@einstein.k12.ec>
2026-02-22 10:16:50 -0500

    Delete flashcard.py

--- flashcard.py
+++
@@ -1,8 +0,0 @@
-class Flashcard:
-    def __init__(self, question, answer):
-        self.question = question
-        self.answer = answer
-
-    def __str__(self):
-        return f"Question: {self.question}, \nAnswer: {self.answer}"
-
